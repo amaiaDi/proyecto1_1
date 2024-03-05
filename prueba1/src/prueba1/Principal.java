759b930396c42db68d8f189270211dcbd4b4c3f4
amaiaDi <aaa@aaa.com>
2024-03-05 13:47:27 +0100

    Segundo commit sobre Principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -3,7 +3,12 @@ package prueba1;
 public class Principal {
 
 	public static void main(String[] args) {
+		
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Adios");
+		System.out.println("Pruebas Git. Se añade Cambio1");
+		
 
 	}
 
