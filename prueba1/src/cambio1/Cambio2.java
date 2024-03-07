f48db28b70113b0d9125a9b5d0d99202be96acb0
amaiaDi <aaa@aaa.com>
2024-03-07 13:51:13 +0100

    Codigo mergeado del commit 0f55r4

--- prueba1/src/cambio1/Cambio2.java
+++ prueba1/src/cambio1/Cambio2.java
@@ -3,9 +3,10 @@ package cambio1;
 public class Cambio2 {
 
 		public static void main(String[] args) {
+			System.out.println("Clase Cambio2 en proyecto1_1");
+			System.out.println("Clase Cambio2 en proyecto1_1");
 			System.out.println("Clase Cambio2 en proyecto1_2");
 			System.out.println("Clase Cambio2 en proyecto1_2");
-
 		}
 
 }
